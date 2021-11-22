7422dc99f8dc86f74dce1221e8df25669dbdcf8d
maxo <maximino33@hotmail.com>
2021-11-21 22:34:22 -0600

    carousel de la seccion conoce nuestro trabajo modificado, con la libreria glider.js

---
+++ js/myScript.js
@@ -0,0 +1,26 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 992,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          itemWidth: 150,
+          duration: 0.25
+        }
+      }
+    ]
+})
+
+const glider = document.querySelector('.glider')
+console.log(glider);
